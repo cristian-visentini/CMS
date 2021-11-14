15feaae28dd62383372f039dc6baaea51f296d20
Cristian Visentini <cristianvisentini@hotmail.com>
2021-11-14 16:53:19 -0300

    criação do registro

--- app/Http/Controllers/Admin/Users/UsersController.php
+++ app/Http/Controllers/Admin/Users/UsersController.php
@@ -3,15 +3,34 @@
 namespace App\Http\Controllers\Admin\Users;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UsersController extends Controller
 {
-    public function index(){
-        return view('login.register');
+    public function index()
+    {
+        return view('admin.register.register');
     }
 
-    public function register(){
+    public function register(Request $request)
+    {
+        $validate = $request->validate([
+            'name' => ['required', 'string'],
+            'email' => ['required', 'email', 'unique:users'],
+            'password' => ['required', 'confirmed'],
+            'password_confirmation' => ['required']
+        ]);
 
+        $user = User::create([
+            'name' => $validate['name'],
+            'email' => $validate['email'],
+            'password' => Hash::make($validate['passwrd']),
+        ]);
+        Auth::login($user);
+
+        return redirect()->route('admin');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Login\LoginController;
+use App\Http\Controllers\Admin\Users\UsersController;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Site\SiteController;
 
@@ -20,28 +21,21 @@ Route::prefix('site')->group(function (){
     Route::get( '/', [SiteController::class, 'index'])->name('site.index');
 });
 
-/*
-Route::middleware('auth')->prefix('admin')->group(function () {
-   // dd('chegou no prefix');
-    Route::get( '/', [AdminController::class, 'index'])->name('admin.index');
-    Route::get( 'login', [LoginController::class, 'login'])->name('admin.login');
-});
-*/
 Route::prefix('admin')->group(function () {
-    // dd('chegou no prefix');
-     Route::get( '/', [AdminController::class, 'index'])->name('admin.index');
-    // Route::get( 'login', [LoginController::class, 'login'])->name('login');
+
+    Route::get( '/', [AdminController::class, 'index'])->name('admin.index');
 
     Route::prefix('login')->group(function () {
-        // dd('chegou no login');
          Route::get( '/', [LoginController::class, 'index'])->name('login');
          Route::post( '/', [LoginController::class, 'login'])->name('login.login');
          Route::post( '/logout', [LoginController::class, 'logout'])->name('login.logout');
      });
+
+     Route::prefix('register')->group(function () {
+        Route::get( '/', [UsersController::class, 'index'])->name('register');
+        Route::post( '/', [UsersController::class, 'register'])->name('register.action');
+    });
+
  });
-
-
-
-
 
 Route::get( '/', [SiteController::class, 'index'])->name('site.index');
